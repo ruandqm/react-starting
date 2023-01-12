51c24fc200aaebcf059bf507978feeb5bfffeedd
ruandqm <ruandqm@gmail.com>
2023-01-11 22:04:19 -0300

    1.4

--- calculator/src/components/Atoms/Input/index.jsx
+++ calculator/src/components/Atoms/Input/index.jsx
@@ -1,7 +1,12 @@
-export default function Input(props) {
+import withHover from "../../HOCs/withHover"
+
+function Input(props) {
     return (
         <div className="col">
-            <input id={props.id} type={props.type} className="form-control"></input>
+            <input id={props.id} type={props.type} onMouseOver={props.onHover} onMouseOut={props.onHover} className="form-control"></input>
+            {props.hover ? <div>Hover</div> : null}
         </div>
     )
 }
+
+export default withHover(Input)
